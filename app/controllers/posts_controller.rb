7b50b174612614e0942312419c88903f3cf872f2
Chris Topher <programmingfire@gmail.com>
2018-12-09 13:12:08 -0600

    First Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+    def index
+
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+    
+    def new
+        
+    end
+
+    def create
+        #render plain: params[:post].inspect
+        @post = Post.new(params[:post_params])
+
+        @post.save
+        redirect_to @post
+    end 
+
+    private def post_params
+        params.require(:post).permit(:title, :body)
+    end
+end
